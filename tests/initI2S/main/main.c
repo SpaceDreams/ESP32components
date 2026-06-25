b3aa0b9f6013684969baae7e36e262eb6c8da4d1
Charles White <charles.white988@gmail.com>
2026-06-25 11:28:20 -1000

    so, something changed, but I'm not sure what. It still looks noisy but it looks less random. I'd like to try saving no and playing the recording.

--- tests/initI2S/main/main.c
+++ tests/initI2S/main/main.c
@@ -6,11 +6,11 @@
 
 /* I2S Digital Microphone Recording Example */
 #include "init_I2S.h"
-#define SINGLE_SAMPLE_SIZE  (INIT_AUDIO_BIT_WIDTH / 8) +1 // I can store in 2 bytes, or 4 bytes but not 3 bytes.
+#define SINGLE_SAMPLE_SIZE  (32 / 8) // I can store in 2 bytes, or 4 bytes but not 3 bytes.
 #define SAMPLE_SIZE         (SINGLE_SAMPLE_SIZE * 1024)
 #define ENABLE_MIC_PIN 14
 
-static uint8_t i2s_readraw_buff[SAMPLE_SIZE];
+static uint32_t i2s_readraw_buff[SAMPLE_SIZE];
 size_t bytes_read;
 
 void stream_audio()
@@ -18,14 +18,14 @@ void stream_audio()
     init_microphone();
     while (true) {
         // Read the RAW samples from the microphone
-        if (i2s_channel_read(rx_handle, i2s_readraw_buff, SAMPLE_SIZE, &bytes_read, 1000) == ESP_OK) {
-            for (int i = 0; i < bytes_read/4; i=i+4) {
-                uint32_t rawbytes = (i2s_readraw_buff[i]<<24) | (i2s_readraw_buff[i+1]<<16) | (i2s_readraw_buff[i+2]<<8);
-                printf("%ld\n", (long int)rawbytes>>8);
-                if (!((i2s_readraw_buff[i+3] == 0xFF) | (i2s_readraw_buff[i+3] == 0))) {
+        if (i2s_channel_read(rx_handle, i2s_readraw_buff, sizeof(i2s_readraw_buff), &bytes_read, 1000) == ESP_OK) {
+            for (int i = 0; i < bytes_read/sizeof(uint32_t); i++) {
+                //uint32_t rawbytes = (i2s_readraw_buff[i]<<24) | (i2s_readraw_buff[i+1]<<16) | (i2s_readraw_buff[i+2]<<8);
+                printf("%ld\n", ((long int)i2s_readraw_buff[i])>>8);
+                if (!(((i2s_readraw_buff[i]&0xFF) == 0xFF) | ((i2s_readraw_buff[i]&0xFF) == 0))) {
                     printf("Error, last byte is: ");
                     for (int j=0; j<8; j++){
-                        printf("%d", (i2s_readraw_buff[i+3] >> (7 - j)) & 1);
+                        printf("%d", (int)((i2s_readraw_buff[i+3] >> (7 - j)) & 1));
                     }
                     printf("\n");
                 }
