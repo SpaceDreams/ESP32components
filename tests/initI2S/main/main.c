5f15dcc8521fbea8e00872a3cd33ab962d3a78b5
Charles White <charles.white988@gmail.com>
2026-07-01 15:37:38 -1000

    So, Now it looks like it's working. But I still have to track the left slot; I would like to use the mask to get rid of that.

--- tests/initI2S/main/main.c
+++ tests/initI2S/main/main.c
@@ -10,20 +10,34 @@
 #include "freertos/FreeRTOS.h" // need for portMAXDELAY
 #include "freertos/task.h" // for portMAXDELAY
 //#define SINGLE_SAMPLE_SIZE  (32 / 8)  // I can store in 2 bytes, or 4 bytes but not 3 bytes.
-#define SAMPLE_SIZE         512 //(SINGLE_SAMPLE_SIZE * 1024) // Was 1024
+#define SAMPLE_SIZE         516 //(SINGLE_SAMPLE_SIZE * 1024) // Was 1024 Using a 24 bit 
 #define ENABLE_MIC_PIN 14
 
-static int32_t i2s_readraw_buff[SAMPLE_SIZE];
+static uint8_t i2s_readraw_buff[SAMPLE_SIZE];
 size_t bytes_read;
 
 void stream_audio()
 {
     init_microphone();
+    // Track whether the current 3-byte block is Left or Right
+    bool is_left_channel = true; 
     while (true) {
         // Read the RAW samples from the microphone
-        if (i2s_channel_read(rx_handle, i2s_readraw_buff, sizeof(uint32_t)*SAMPLE_SIZE, &bytes_read, portMAX_DELAY) == ESP_OK) {
-            for (int i = 0; i < bytes_read/sizeof(uint32_t); i+=2) 
-                printf("Raw Integer: %ld | Raw Hex: 0x%08lX\n", i2s_readraw_buff[i], i2s_readraw_buff[i]);
+        if (i2s_channel_read(rx_handle, i2s_readraw_buff, SAMPLE_SIZE, &bytes_read, portMAX_DELAY) == ESP_OK) {
+            //size_t complete_bytes = bytes_read - (bytes_read % 3);
+            for (int i = 0; i < bytes_read; i+=3) {
+                int32_t value = 0;
+                for (int j = 0; j<3; j++)
+                    value |= i2s_readraw_buff[i+j]<<8*j;
+                //printf("Raw Integer: %ld | Raw Hex: 0x%08lX\n", value>>8, value>>8);
+                if (value & 0x00800000) {
+                       value |= 0xFF000000; 
+                }
+                if(is_left_channel)
+                    printf("%ld\n",value);
+                    //printf("Raw Integer: %ld | Raw Hex: 0x%08lX\n", value>>8, value>>8);
+                is_left_channel = !is_left_channel;
+            }
                 //printf("%ld \n", i2s_readraw_buff[i]>>8);
         } else {
             printf("Read Failed!\n");
